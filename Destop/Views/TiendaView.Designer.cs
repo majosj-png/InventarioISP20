8873726f325aeda398464bfcc13748e0b0eafe89
majosj-png <marialongoni984@gmail.com>
2026-06-30 17:41:24 -0300

    Mejoras en diseño y usabilidad de la interfaz
    Se ajustaron las ubicaciones, tamaños y márgenes de múltiples controles en la interfaz de usuario para adaptarse a un diseño más amplio y espacioso.
    
    - Actualización de `Location`, `Size` y `Margin` en controles como `tabControlLista`, `tabPageLista`, botones y campos de texto.
    - Cambios en índices de tabulación (`TabIndex`) para mejorar la navegación.
    - Ajustes en etiquetas para mayor legibilidad y consistencia visual.
    - Mejoras en `dataGridTiendas`, añadiendo propiedades como `RowHeadersWidth` y `SelectionMode`.
    - Escalado actualizado (`AutoScaleDimensions`) y tamaño de ventana principal (`ClientSize`) ampliado.
    - Cambios enfocados en mejorar la experiencia de usuario y la funcionalidad general.

--- Destop/Views/TiendaView.Designer.cs
+++ Destop/Views/TiendaView.Designer.cs
@@ -61,11 +61,12 @@
             // 
             tabControlLista.Controls.Add(tabPageLista);
             tabControlLista.Controls.Add(tabPageAgregarEditar);
-            tabControlLista.Location = new Point(9, 45);
+            tabControlLista.Location = new Point(10, 60);
+            tabControlLista.Margin = new Padding(3, 4, 3, 4);
             tabControlLista.Name = "tabControlLista";
             tabControlLista.SelectedIndex = 0;
-            tabControlLista.Size = new Size(783, 394);
-            tabControlLista.TabIndex = 3;
+            tabControlLista.Size = new Size(895, 525);
+            tabControlLista.TabIndex = 1;
             // 
             // tabPageLista
             // 
@@ -76,10 +77,11 @@
             tabPageLista.Controls.Add(dataGridTiendas);
             tabPageLista.Controls.Add(txtBusqueda);
             tabPageLista.Controls.Add(labelBuscar);
-            tabPageLista.Location = new Point(4, 24);
+            tabPageLista.Location = new Point(4, 29);
+            tabPageLista.Margin = new Padding(3, 4, 3, 4);
             tabPageLista.Name = "tabPageLista";
-            tabPageLista.Padding = new Padding(3);
-            tabPageLista.Size = new Size(775, 366);
+            tabPageLista.Padding = new Padding(3, 4, 3, 4);
+            tabPageLista.Size = new Size(887, 492);
             tabPageLista.TabIndex = 0;
             tabPageLista.Text = "Lista";
             tabPageLista.UseVisualStyleBackColor = true;
@@ -89,10 +91,11 @@
             btnBuscar.IconChar = FontAwesome.Sharp.IconChar.None;
             btnBuscar.IconColor = Color.Black;
             btnBuscar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            btnBuscar.Location = new Point(680, 38);
+            btnBuscar.Location = new Point(777, 51);
+            btnBuscar.Margin = new Padding(3, 4, 3, 4);
             btnBuscar.Name = "btnBuscar";
-            btnBuscar.Size = new Size(75, 23);
-            btnBuscar.TabIndex = 7;
+            btnBuscar.Size = new Size(86, 31);
+            btnBuscar.TabIndex = 3;
             btnBuscar.Text = "Buscar";
             btnBuscar.UseVisualStyleBackColor = true;
             btnBuscar.Click += btnBuscar_Click;
@@ -102,9 +105,10 @@
             btnEliminar.IconChar = FontAwesome.Sharp.IconChar.None;
             btnEliminar.IconColor = Color.Black;
             btnEliminar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            btnEliminar.Location = new Point(677, 175);
+            btnEliminar.Location = new Point(774, 233);
+            btnEliminar.Margin = new Padding(3, 4, 3, 4);
             btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new Size(75, 23);
+            btnEliminar.Size = new Size(86, 31);
             btnEliminar.TabIndex = 6;
             btnEliminar.Text = "Eliminar";
             btnEliminar.UseVisualStyleBackColor = true;
@@ -115,9 +119,10 @@
             btnModificar.IconChar = FontAwesome.Sharp.IconChar.None;
             btnModificar.IconColor = Color.Black;
             btnModificar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            btnModificar.Location = new Point(677, 129);
+            btnModificar.Location = new Point(774, 172);
+            btnModificar.Margin = new Padding(3, 4, 3, 4);
             btnModificar.Name = "btnModificar";
-            btnModificar.Size = new Size(75, 23);
+            btnModificar.Size = new Size(86, 31);
             btnModificar.TabIndex = 5;
             btnModificar.Text = "Modificar";
             btnModificar.UseVisualStyleBackColor = true;
@@ -128,9 +133,10 @@
             btnNuevo.IconChar = FontAwesome.Sharp.IconChar.None;
             btnNuevo.IconColor = Color.Black;
             btnNuevo.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            btnNuevo.Location = new Point(677, 80);
+            btnNuevo.Location = new Point(774, 107);
+            btnNuevo.Margin = new Padding(3, 4, 3, 4);
             btnNuevo.Name = "btnNuevo";
-            btnNuevo.Size = new Size(75, 23);
+            btnNuevo.Size = new Size(86, 31);
             btnNuevo.TabIndex = 4;
             btnNuevo.Text = "Nuevo";
             btnNuevo.UseVisualStyleBackColor = true;
@@ -141,25 +147,29 @@
             dataGridTiendas.AllowUserToAddRows = false;
             dataGridTiendas.AllowUserToDeleteRows = false;
             dataGridTiendas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridTiendas.Location = new Point(18, 92);
+            dataGridTiendas.Location = new Point(21, 123);
+            dataGridTiendas.Margin = new Padding(3, 4, 3, 4);
             dataGridTiendas.Name = "dataGridTiendas";
             dataGridTiendas.ReadOnly = true;
-            dataGridTiendas.Size = new Size(631, 269);
-            dataGridTiendas.TabIndex = 3;
+            dataGridTiendas.RowHeadersWidth = 51;
+            dataGridTiendas.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridTiendas.Size = new Size(721, 359);
+            dataGridTiendas.TabIndex = 2;
             // 
             // txtBusqueda
             // 
-            txtBusqueda.Location = new Point(68, 39);
+            txtBusqueda.Location = new Point(78, 52);
+            txtBusqueda.Margin = new Padding(3, 4, 3, 4);
             txtBusqueda.Name = "txtBusqueda";
-            txtBusqueda.Size = new Size(581, 23);
+            txtBusqueda.Size = new Size(663, 27);
             txtBusqueda.TabIndex = 1;
             // 
             // labelBuscar
             // 
             labelBuscar.AutoSize = true;
-            labelBuscar.Location = new Point(14, 46);
+            labelBuscar.Location = new Point(16, 61);
             labelBuscar.Name = "labelBuscar";
-            labelBuscar.Size = new Size(42, 15);
+            labelBuscar.Size = new Size(52, 20);
             labelBuscar.TabIndex = 0;
             labelBuscar.Text = "Buscar";
             // 
@@ -177,93 +187,99 @@
             tabPageAgregarEditar.Controls.Add(labelMarca);
             tabPageAgregarEditar.Controls.Add(btnCancelar);
             tabPageAgregarEditar.Controls.Add(btnGuardar);
-            tabPageAgregarEditar.Location = new Point(4, 24);
+            tabPageAgregarEditar.Location = new Point(4, 29);
+            tabPageAgregarEditar.Margin = new Padding(3, 4, 3, 4);
             tabPageAgregarEditar.Name = "tabPageAgregarEditar";
-            tabPageAgregarEditar.Padding = new Padding(3);
-            tabPageAgregarEditar.Size = new Size(775, 366);
+            tabPageAgregarEditar.Padding = new Padding(3, 4, 3, 4);
+            tabPageAgregarEditar.Size = new Size(887, 492);
             tabPageAgregarEditar.TabIndex = 1;
             tabPageAgregarEditar.Text = "Agregar/Editar";
             tabPageAgregarEditar.UseVisualStyleBackColor = true;
             // 
             // txtEstacion
             // 
-            txtEstacion.Location = new Point(105, 269);
+            txtEstacion.Location = new Point(120, 359);
+            txtEstacion.Margin = new Padding(3, 4, 3, 4);
             txtEstacion.Name = "txtEstacion";
-            txtEstacion.Size = new Size(450, 23);
-            txtEstacion.TabIndex = 11;
+            txtEstacion.Size = new Size(514, 27);
+            txtEstacion.TabIndex = 9;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(38, 274);
+            label1.Location = new Point(43, 365);
             label1.Name = "label1";
-            label1.Size = new Size(51, 15);
-            label1.TabIndex = 10;
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 4;
             label1.Text = "Estación";
             // 
             // txtPrecio
             // 
-            txtPrecio.Location = new Point(105, 213);
+            txtPrecio.Location = new Point(120, 284);
+            txtPrecio.Margin = new Padding(3, 4, 3, 4);
             txtPrecio.Name = "txtPrecio";
-            txtPrecio.Size = new Size(453, 23);
-            txtPrecio.TabIndex = 9;
+            txtPrecio.Size = new Size(517, 27);
+            txtPrecio.TabIndex = 8;
             // 
             // txtTipoArticulo
             // 
-            txtTipoArticulo.Location = new Point(102, 144);
+            txtTipoArticulo.Location = new Point(117, 192);
+            txtTipoArticulo.Margin = new Padding(3, 4, 3, 4);
             txtTipoArticulo.Name = "txtTipoArticulo";
-            txtTipoArticulo.Size = new Size(453, 23);
-            txtTipoArticulo.TabIndex = 8;
+            txtTipoArticulo.Size = new Size(517, 27);
+            txtTipoArticulo.TabIndex = 7;
             // 
             // txtTipoCliente
             // 
-            txtTipoCliente.Location = new Point(102, 87);
+            txtTipoCliente.Location = new Point(117, 116);
+            txtTipoCliente.Margin = new Padding(3, 4, 3, 4);
             txtTipoCliente.Name = "txtTipoCliente";
-            txtTipoCliente.Size = new Size(453, 23);
-            txtTipoCliente.TabIndex = 7;
+            txtTipoCliente.Size = new Size(517, 27);
+            txtTipoCliente.TabIndex = 6;
             // 
             // txtMarca
             // 
-            txtMarca.Location = new Point(102, 43);
+            txtMarca.Location = new Point(117, 57);
+            txtMarca.Margin = new Padding(3, 4, 3, 4);
             txtMarca.Name = "txtMarca";
-            txtMarca.Size = new Size(453, 23);
-            txtMarca.TabIndex = 6;
+            txtMarca.Size = new Size(517, 27);
+            txtMarca.TabIndex = 5;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(38, 221);
+            label4.Location = new Point(43, 295);
             label4.Name = "label4";
-            label4.Size = new Size(40, 15);
-            label4.TabIndex = 5;
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 3;
             label4.Text = "Precio";
             label4.Click += label4_Click;
             // 
             // labelTipoArticulo
             // 
             labelTipoArticulo.AutoSize = true;
-            labelTipoArticulo.Location = new Point(21, 152);
+            labelTipoArticulo.Location = new Point(24, 203);
             labelTipoArticulo.Name = "labelTipoArticulo";
-            labelTipoArticulo.Size = new Size(75, 15);
-            labelTipoArticulo.TabIndex = 4;
+            labelTipoArticulo.Size = new Size(95, 20);
+            labelTipoArticulo.TabIndex = 2;
             labelTipoArticulo.Text = "Tipo_articulo";
             // 
             // labeltipoCliente
             // 
             labeltipoCliente.AutoSize = true;
-            labeltipoCliente.Location = new Point(21, 94);
+            labeltipoCliente.Location = new Point(24, 125);
             labeltipoCliente.Name = "labeltipoCliente";
-            labeltipoCliente.Size = new Size(70, 15);
-            labeltipoCliente.TabIndex = 3;
+            labeltipoCliente.Size = new Size(89, 20);
+            labeltipoCliente.TabIndex = 1;
             labeltipoCliente.Text = "Tipo_cliente";
             // 
             // labelMarca
             // 
             labelMarca.AutoSize = true;
-            labelMarca.Location = new Point(33, 46);
+            labelMarca.Location = new Point(38, 61);
             labelMarca.Name = "labelMarca";
-            labelMarca.Size = new Size(40, 15);
-            labelMarca.TabIndex = 2;
+            labelMarca.Size = new Size(50, 20);
+            labelMarca.TabIndex = 0;
             labelMarca.Text = "Marca";
             // 
             // btnCancelar
@@ -271,10 +287,11 @@
             btnCancelar.IconChar = FontAwesome.Sharp.IconChar.None;
             btnCancelar.IconColor = Color.Black;
             btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            btnCancelar.Location = new Point(680, 90);
+            btnCancelar.Location = new Point(777, 120);
+            btnCancelar.Margin = new Padding(3, 4, 3, 4);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(75, 23);
-            btnCancelar.TabIndex = 1;
+            btnCancelar.Size = new Size(86, 31);
+            btnCancelar.TabIndex = 11;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
             btnCancelar.Click += btnCancelar_Click;
@@ -284,10 +301,11 @@
             btnGuardar.IconChar = FontAwesome.Sharp.IconChar.None;
             btnGuardar.IconColor = Color.Black;
             btnGuardar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            btnGuardar.Location = new Point(680, 29);
+            btnGuardar.Location = new Point(777, 39);
+            btnGuardar.Margin = new Padding(3, 4, 3, 4);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(75, 23);
-            btnGuardar.TabIndex = 0;
+            btnGuardar.Size = new Size(86, 31);
+            btnGuardar.TabIndex = 10;
             btnGuardar.Text = "Guardar";
             btnGuardar.UseVisualStyleBackColor = true;
             btnGuardar.Click += btnGuardar_Click;
@@ -296,19 +314,20 @@
             // 
             labelArticulos.AutoSize = true;
             labelArticulos.Font = new Font("Segoe UI", 16F, FontStyle.Bold | FontStyle.Italic);
-            labelArticulos.Location = new Point(24, 12);
+            labelArticulos.Location = new Point(27, 16);
             labelArticulos.Name = "labelArticulos";
-            labelArticulos.Size = new Size(105, 30);
+            labelArticulos.Size = new Size(131, 37);
             labelArticulos.TabIndex = 2;
             labelArticulos.Text = "Articulos";
             // 
             // TiendaView
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(tabControlLista);
             Controls.Add(labelArticulos);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "TiendaView";
             Text = "TiendaView";
             tabControlLista.ResumeLayout(false);
