85ea21214901224d1ddf6e90895cc92a35aca3a5
majosj-png <marialongoni984@gmail.com>
2026-07-02 11:26:33 -0300

    Permitir valores nulos en propiedades de Tienda
    Se ha actualizado la clase `Tienda` para permitir valores nulos en las propiedades `id` y `created_at`, cambiando sus tipos a `int?` y `DateTime?`, respectivamente. Esto mejora la flexibilidad al manejar datos opcionales.

--- Destop/Models/Tienda.cs
+++ Destop/Models/Tienda.cs
@@ -3,8 +3,8 @@ namespace Desktop.Models
 {
     public class Tienda
     {
-        public int id { get; set; }
-        public DateTime created_at { get; set; }
+        public int? id { get; set; }
+        public DateTime? created_at { get; set; }
         public string marca { get; set; }
         public float precio { get; set; }
         public string tipo_cliente { get; set; }
